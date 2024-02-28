5e206df427db8cf2c1f8f72d06a9bf1c6f2816c5
Tomás Barrientos Stocker <tobarrientos2@gmail.com>
2024-02-28 11:36:10 -0300

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,12 @@
 	import Header from "../components/Header.svelte";
 	import "./styles.css";
 	import { page } from "$app/stores";
-	
-
 </script>
 
 <svelte:head>
 	<title>{"Inicio"} | </title>
 	<meta content="width=device-width, initial-scale=1" name="viewport" />
-
+	
 </svelte:head>
 
 <Header  />
